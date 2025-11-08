a76d541d6f29e6f066c1cd948c3734bf982a9d52
Michael Smith <throwawayclover@gmail.com>
2025-11-08 09:18:43 -0600

    chore: sort index.ts for core

--- packages/time-sync/src/index.ts
+++ packages/time-sync/src/index.ts
@@ -1,10 +1,10 @@
 // Not using wildcard syntax to make final dependencies more obvious
 export {
-    TimeSync,
-    newReadonlyDate,
-    REFRESH_IDLE,
-    REFRESH_ONE_HOUR,
-    REFRESH_ONE_MINUTE,
-    REFRESH_ONE_SECOND,
-    type SubscriptionHandshake,
-} from "./TimeSync"
+	newReadonlyDate,
+	REFRESH_IDLE,
+	REFRESH_ONE_HOUR,
+	REFRESH_ONE_MINUTE,
+	REFRESH_ONE_SECOND,
+	type SubscriptionHandshake,
+	TimeSync,
+} from "./TimeSync";
