9960f1a2636d60cffcd335f32d3f41abd28b25e1
Michael Smith <throwawayclover@gmail.com>
2025-11-20 09:31:45 -0500

    fix: clean up exports

--- packages/time-sync-react/src/index.ts
+++ packages/time-sync-react/src/index.ts
@@ -1,4 +1,15 @@
-export * from "../../time-sync/src";
+// Currently erring on the side of NOT exposing the base TimeSync implementation
+// (or any related types for setting it up) from this package. If you have a
+// React project, you probably don't ever need the raw TimeSync aside from some
+// incidental escape hatches. But useTimeSyncRef takes care of all of that
+export {
+	type InvalidateStateOptions,
+	type NotificationBehavior,
+	ReadonlyDate,
+	refreshRates,
+	type Snapshot,
+	type SubscriptionHandshake,
+} from "../../time-sync/src";
 export {
 	TimeSyncProvider,
 	type TimeSyncProviderProps,
